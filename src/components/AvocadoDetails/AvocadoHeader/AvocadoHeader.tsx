814fa5ea612d16c155da79820621f64b0029ffcd
Pedro Andrés Chaparro Quintero <pedroandreschaparro@gmail.com>
2023-01-03 13:52:08 -0500

    :iphone: Little improvements on responsive layout

--- src/components/AvocadoDetails/AvocadoHeader/AvocadoHeader.tsx
+++ src/components/AvocadoDetails/AvocadoHeader/AvocadoHeader.tsx
@@ -10,8 +10,8 @@ interface IProps {
 const AvocadoHeader = ({ image, name, price, sku }: IProps) => {
   return (
     <div className="grid md:grid-cols-2 gap-4">
-      <img src={image} alt={name} />
-      <div className="flex flex-col items-start gap-2">
+      <img className="mx-auto" src={image} alt={name} />
+      <div className="flex flex-col items-center md:items-start gap-2 mb-8">
         <h1 className="text-xl font-bold">{name}</h1>
         <p className="text-neutral-600">$ {price}</p>
         <p className="text-sm inline-block bg-neutral-300 py-1 px-2 rounded-md">
