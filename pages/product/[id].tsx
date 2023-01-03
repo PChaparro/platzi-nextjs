6cb704b3ff2a2769926e7a05170593e0bb254869
Pedro Andrés Chaparro Quintero <pedroandreschaparro@gmail.com>
2023-01-03 10:05:41 -0500

    :sparkles: Avocado details on `/avocados/{id}` route

--- pages/product/[id].tsx
+++ pages/product/[id].tsx
@@ -1,13 +1,27 @@
 import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
 
 const Product = () => {
+  const [avocado, setAvocado] = useState<TProduct>(null)
+
   // Access the param from the url
   const router = useRouter();
   const { id } = router.query;
 
+  useEffect(() => {
+    const getAvocado = async () => {
+      const response = await window.fetch(`/api/avocados/${id}`)
+      const data = await response.json()
+      setAvocado(data)
+    }
+
+    getAvocado()
+  }, [])
+
   return (
     <div>
-      <h1>Product: [{id}]</h1>
+      <h1>Product: [{avocado.id}]</h1>
+      <h2>{avocado.name}</h2>
     </div>
   );
 };
