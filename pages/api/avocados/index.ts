358efe3c80fec09889c550f6b5ec3e566c2d6cf0
Pedro Andrés Chaparro Quintero <pedroandreschaparro@gmail.com>
2023-01-03 09:43:45 -0500

    :sparkles: First endpoint
    Endpoint to get all the avocados

---
+++ pages/api/avocados/index.ts
@@ -0,0 +1,14 @@
+import { IncomingMessage, ServerResponse } from "http";
+
+import DB from "../../../database/db";
+const db = new DB();
+
+const allAvocados = async (req: IncomingMessage, res: ServerResponse) => {
+  const avocados = await db.getAll();
+
+  res.statusCode = 200;
+  res.setHeader("Content-type", "application/json");
+  res.end(JSON.stringify({ length: avocados.length, data: avocados }));
+};
+
+export default allAvocados;
