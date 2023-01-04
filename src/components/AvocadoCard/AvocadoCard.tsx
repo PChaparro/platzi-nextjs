5c5e2c10b075d77aeca700a4e7b3656a4672f895
Pedro Andrés Chaparro Quintero <pedroandreschaparro@gmail.com>
2023-01-04 12:35:05 -0500

    :zap: Use `Image` component on home page

--- src/components/AvocadoCard/AvocadoCard.tsx
+++ src/components/AvocadoCard/AvocadoCard.tsx
@@ -1,4 +1,5 @@
 import Link from "next/link";
+import Image from "next/image";
 
 interface IProps {
   product: TProduct;
@@ -8,7 +9,13 @@ const AvocadoCard = ({ product }: IProps) => {
   return (
     <Link href={`/avocado/${product.id}`}>
       <article className="h-full text-center border-2 border-neutral-200 rounded-lg p-4 hover:scale-105 hover:shadow-lg transition-all">
-        <img src={product.image} alt={product.name} />
+        <Image
+          className="mx-auto"
+          src={product.image}
+          alt={product.name}
+          width={198}
+          height={198}
+        />
         <h2 className="font-bold text-lg  my-2">{product.name}</h2>
         <p className="text-neutral-600">$ {product.price}</p>
       </article>
