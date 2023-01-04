f9f26a01474090b922320459cd1e857f0c2744c1
Pedro Andrés Chaparro Quintero <pedroandreschaparro@gmail.com>
2023-01-04 09:52:33 -0500

    :lipstick: Fix cards height

--- src/components/AvocadoCard/AvocadoCard.tsx
+++ src/components/AvocadoCard/AvocadoCard.tsx
@@ -7,9 +7,9 @@ interface IProps {
 const AvocadoCard = ({ product }: IProps) => {
   return (
     <Link href={`/product/${product.id}`}>
-      <article className="border-2 border-neutral-200 rounded-lg p-4 hover:scale-105 hover:shadow-lg transition-all">
+      <article className="h-full text-center border-2 border-neutral-200 rounded-lg p-4 hover:scale-105 hover:shadow-lg transition-all">
         <img src={product.image} alt={product.name} />
-        <h2 className="font-bold text-lg my-2">{product.name}</h2>
+        <h2 className="font-bold text-lg  my-2">{product.name}</h2>
         <p className="text-neutral-600">$ {product.price}</p>
       </article>
     </Link>
